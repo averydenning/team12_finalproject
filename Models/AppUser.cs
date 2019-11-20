89bf8630ef4209c0116dd1d06cdf3576c0c7c15e
Avery Denning <averykdenning@utexas.edu>
2019-11-19 20:58:20 -0600

    Add model classes and begin correcting name

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -7,7 +7,6 @@ namespace Final_Project_Team12.Models
 {
     public class AppUser : IdentityUser
     {
-
         //First name is provided as an example
         [Required(ErrorMessage = "First name is required.")]
         [Display(Name = "First Name")]
@@ -21,6 +20,8 @@ namespace Final_Project_Team12.Models
         [Required(ErrorMessage = "Last name is required.")]
         [Display(Name = "Last Name")]
         public String LastName { get; set; }
+
+        //NEED TO ADD BIRTH DAY - required - allows us to track age for IRA account eligibility 
 
         //
         [Required(ErrorMessage = "Street is required.")]
@@ -42,8 +43,12 @@ namespace Final_Project_Team12.Models
         [Display(Name = "Zip code")]
         public String Zip { get; set; }
 
-        [Display(Name = "Accounts")]
-        public List <BankAccount> Accounts { get; set; }
+        [Display(Name = "Standard Accounts")]
+        public List<StandardAccount> StandardAccounts { get; set; }
+
+        // Having an issue with this one to one relationship. Need to go to office hours to figure it out.
+        //[Display(Name = "IRA Account")]
+        //public IRAAccount IRAAccount { get; set; }
 
         [Display(Name = "Is user active?")]
         public Boolean IsActive { get; set; }
